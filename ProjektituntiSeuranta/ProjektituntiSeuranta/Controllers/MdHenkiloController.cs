5c9ebfea64276e0836e079b12c46b2b16b42e904
Bo Furustam <bo.furustam@stud.pointcollege.fi>
2018-01-19 10:24:45 +0200

    actionlinkit kaikille sivuille

---
+++ ProjektituntiSeuranta/ProjektituntiSeuranta/Controllers/MdHenkiloController.cs
@@ -0,0 +1,29 @@
+﻿using ProjektituntiSeuranta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjektituntiSeuranta.Controllers
+{
+    public class MdHenkiloController : Controller
+    {
+        // GET: MdHenkilo
+        public ActionResult Index()
+        {
+            AsiakastietokantaEntities entities = new AsiakastietokantaEntities();
+            try
+            {
+                List<Henkilot> model = entities.Henkilots.ToList();
+                return View(model);
+            }
+
+            finally
+            {
+                entities.Dispose();
+            }
+            
+        }
+    }
+}
